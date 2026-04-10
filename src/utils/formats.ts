5368baac569e3bededf11dae626283e9189f3cf9
duyaivy <quocduy0322@gmail.com>
2026-04-10 17:21:04 +0700

    fix: format vietnamese to ascii

---
+++ src/utils/formats.ts
@@ -0,0 +1,7 @@
+export const vietnameseToAscii = (input: string): string => {
+  return input
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/đ/g, 'd')
+    .replace(/Đ/g, 'D');
+};
