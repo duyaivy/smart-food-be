347fd08f51ceac671a31f0ef0dbecbb014722029
Long Đặng Hữu <danghuulong394@gmail.com>
2026-05-17 21:01:23 +0700

    feat: add nutrition overview APIs

---
+++ src/models/interfaces/nutrition.interface.ts
@@ -0,0 +1,32 @@
+export type NutritionMacro = {
+  calories: number;
+  protein: number;
+  carb: number;
+  fat: number;
+};
+
+export type DailyMealNutrition = NutritionMacro & {
+  mealType: string;
+};
+
+export type DailyNutritionResult = {
+  date: string;
+  total: NutritionMacro;
+  meals: DailyMealNutrition[];
+};
+
+export type WeeklyDailyTotal = NutritionMacro & {
+  date: string;
+};
+
+export type WeeklyNutritionResult = {
+  week: string;
+  dailyAverage: NutritionMacro;
+  dailyTotals: WeeklyDailyTotal[];
+};
+
+export type RemainingNutritionResult = {
+  date: string;
+  remaining: NutritionMacro;
+  percentageConsumed: NutritionMacro;
+};
